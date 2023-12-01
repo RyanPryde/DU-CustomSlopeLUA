c650ab153a1ed66cd5517a4a70550aae020dad42
RyanPryde <139646801+RyanPryde@users.noreply.github.com>
2023-12-02 00:06:27 +0100

    Update Slope.lua

--- Slope.lua
+++ Slope.lua
@@ -5,25 +5,17 @@ cx, cy = getCursor()
 text = createLayer()
 font = loadFont('Play',12)
 font2 = loadFont('Play',40)
--------------------------------------------------------------------
---EDIT Line Points below:
---startpoint: 0/0 is bottom left corner
-AX = 0
-AY = 613
-
-
---endpoint: 1024/0 is top right corner
-BX = 1024
-BY = 0
--------------------------------------------------------------------
 setNextStrokeWidth(layer,1)--Stroke with
 setNextStrokeColor(layer,0, 3.4, 5, 1)--(layer,R,G,B,Transparency)
-addLine(layer,AX,AY,BX,BY)
+
+if pointA and pointB then
+    addLine(layer,pointA[1],pointA[2],pointB[1],pointB[2])
+end
 --resolution marker:
 M1 = "0"
 M2 = "613"
 M3 = "1024"
-Desc = "Custom Slope LUA by RyanPryde | Hadron"
+Desc = "Custom Slope LUA by RyanPryde & Wolfram"
 addText(text,font,M1,5,20)
 addText(text,font,M2,10,600)
 addText(text,font,M3,970,20)
@@ -34,18 +26,39 @@ addText(text,font2,"ry: "..math.floor(cy).."",500,50)
 cursorlayer = createLayer()
 cursorlayer2 = createLayer()
 cx, cy = getCursor() 
-cursor_image = loadImage("assets.prod.novaquark.com/102348/a6ad4ff3-372f-46f6-8e2c-86aa0c54f3a3.png")
+
 function drawCursor ()
 if getCursorDown() then
         setDefaultStrokeColor(cursorlayer, Shape_Line, 3, 3, 3, 1)
         setDefaultFillColor(cursorlayer, Shape_Polygon, 3, 3, 3, 1)
     end
+    if getCursorReleased() then
+        if not pointA then
+            pointA = { cx, cy }
+        elseif not pointB then
+            pointB = { cx, cy }
+        else
+            pointA = nil
+            pointB = nil
+        end
+    end
     if cx < 0 then return end
         addTriangle(cursorlayer,cx ,cy ,cx + 17.5, cy + 20, cx -2.5, cy + 25)
         setNextStrokeWidth(cursorlayer,3)
         addLine(cursorlayer,cx + 8, cy + 22.5, cx + 12, cy + 35)
-        addText(cursorlayer,font,"rx: "..math.floor(cx).."",cx+20,cy+30)--RX TEXT
-        addText(cursorlayer,font,"ry: "..math.floor(cy).."",cx+20,cy+40)--RY TEXT
+
+        local tooltip = nil
+        if not pointA then
+            tooltip = 'Click to set slope start'
+        elseif not pointB then
+            tooltip = 'Click to set slope end'
+        else
+            tooltip = 'Click to clear slope'
+        end
+
+        addText(cursorlayer,font,tooltip,cx+20,cy+30)--Tooltip
+        addText(cursorlayer,font,"rx: "..math.floor(cx).."",cx+20,cy+40)--RX TEXT
+        addText(cursorlayer,font,"ry: "..math.floor(cy).."",cx+20,cy+50)--RY TEXT
 end
 drawCursor()
 --
